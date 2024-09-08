9c8f85d5ff96a6f61555b7db0dd25aa8101a1221
Shyam G. Pradhan <2018bcs509@sggs.ac.in>
2024-09-08 15:04:23 +0530

    Create entrypoint.sh
    Added docker file to create jar

---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+set -e
+
+print_message() {
+  echo "========================================"
+  echo "$1"
+  echo "========================================"
+}
+
+print_message "Cleaning the project"
+mvn clean
+
+print_message "Compiling the project"
+mvn compile
+
+print_message "Running tests"
+mvn test
+
+print_message "Packaging the project"
+mvn package
+
+ls /load-balancer/target
+
+chmod -R 777 /target
+cp /load-balancer/target/app-0.0.1-SNAPSHOT.jar /target/app.jar
