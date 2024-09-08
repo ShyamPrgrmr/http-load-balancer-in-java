24a0405663f9c50ef6733757ed5dd75ee67b0514
shyamprgrmr <2018bcs509@sggs.ac.in>
2024-09-08 16:11:49 +0530

    adding build config in runbuild.sh

--- runbuild.sh
+++ runbuild.sh
@@ -1,3 +1,40 @@
+
+while getopts v:u:p:r: flag
+do
+    case "${flag}" in
+        r) repository=${OPTARG};;
+        v) version=${OPTARG};;
+        u) user=${OPTARG};;
+        p) password=${OPTARG};;
+    esac
+done
+
+
+if [ -z "$repository" ]; then
+  echo "Dockerhub repository name is not provided (Use -r while running runbuild.sh)"
+else
+  echo "Docker Hub Repository : $repository"
+fi
+
+if [ -z "$version" ]; then
+  echo "Version is not provided (Use -v while running runbuild.sh)"
+else
+  echo "App Version Name : $version"
+fi
+
+if [ -z "$user" ]; then
+  echo "Username is not provided (Use -u while running runbuild.sh)"
+else
+  echo "Userame : $user"
+fi
+
+if [ -z "$password" ]; then
+  echo "Password is not provided (Use -u while running runbuild.sh)"
+else
+  echo "Login with password"
+fi
+
+
 print_messages(){
 
 echo "======================================"
@@ -17,9 +54,17 @@ chmod -R 775 /target/app.jar
 
 cp /target/app.jar ./builds/app.jar
 
-print_massages "Building runnnable image - In Progress"
+print_massages "Building docker image - In Progress"
 
 cd builds
-docker build -t load-balancer-app:$(app_version) .
+docker build -t $repository/load-balancer-app:$version .
 
-print_massages "Building runnnable image - Completed"
+print_massages "Building docker image - Completed"
+
+
+print_massages "Pushing image into repository - In Progress"
+
+docker login --username $user --password $password
+docker push $repository/load-balancer-app:$version
+
+print_massages "Pushing image into repository - In Progress"
