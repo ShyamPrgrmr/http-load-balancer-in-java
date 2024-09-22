a2815fe00bdefd1ea06b5106d0e6fd412477c229
Shyam G. Pradhan <www.shyam.pradhan@gmail.com>
2024-09-22 11:54:31 +0530

    Update runbuild.sh

--- runbuild.sh
+++ runbuild.sh
@@ -93,7 +93,7 @@ echo "Y" | docker builder prune
 docker builder prune
 
 docker build -t load-balancer-app-builder-$version:latest .
-docker run --name test --rm  -v $workdir/builds:/target -v $workdir/app:/load-balancer  load-balancer-app-builder-$version:latest
+docker run --name test --rm  -v $workdir/builds:/target  load-balancer-app-builder-$version:latest
 
 
 print_messages "Building jar file - Completed"
